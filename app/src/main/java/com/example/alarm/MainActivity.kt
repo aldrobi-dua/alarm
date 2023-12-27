cef9aa19c7567cad96855c0bdfb91d70de607ab3
khaledalsaleh12 <khaledalsaleh30@gmail.com>
2023-12-27 22:07:27 +0300

    ana sayfa görev listesi ve görev ekle butonu yapıldı. Yeni görev ekle sayfasının az bir kısmı yapılıd.

--- app/src/main/java/com/example/alarm/MainActivity.kt
+++ app/src/main/java/com/example/alarm/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.alarm
-
+import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.alarm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val addTaskButton = binding.addTaskButton
+        addTaskButton.setOnClickListener {
+        }
     }
 }
