9ccb0e20881664aaa96e2b84ca1328ae2eea7994
khaledalsaleh12 <khaledalsaleh30@gmail.com>
2023-12-31 16:02:47 +0300

    signın signup yapıldı

--- app/src/main/java/com/example/alarm/MainActivity.kt
+++ app/src/main/java/com/example/alarm/MainActivity.kt
@@ -1,29 +1,23 @@
 package com.example.alarm
+
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.alarm.databinding.ActivityMainBinding
-import android.widget.Button
-import android.widget.TextView
-
-
+import androidx.appcompat.app.AppCompatActivity
+import com.example.alarm.databinding.GorevekleBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: GorevekleBinding
 
-    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+        binding = GorevekleBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         val addTaskButton = binding.addTaskButton
         addTaskButton.setOnClickListener {
-            val intent =  Intent(this, AddNewTaskActivity::class.java )
+            val intent = Intent(this, AddNewTaskActivity::class.java)
             startActivity(intent)
-
-
-            }
         }
     }
-
+}
